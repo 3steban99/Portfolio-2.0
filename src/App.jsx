a2507e0bd1dfc060c1a66e3976779603a6b6b81a
3steban99 <barrionuevo99@outlook.com>
2023-11-06 15:13:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavPortfolio } from './components/navPortfolio'
+
+
+function App() {
+
+
+  return (
+    <>
+      <NavPortfolio />
+      <h1>hola</h1>
+    </>
+  )
+}
+
+export default App
